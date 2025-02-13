7564f160a19550953584dba0d6f150daf86af807
Tedd OKANO <okano.akifumi@gmail.com>
2025-02-14 05:11:26 +0900

    UI improvement

--- AFE_UIM_NAFE13388_A_custom_gain_FRDM_MCXN947/source/coeffs.cpp
+++ AFE_UIM_NAFE13388_A_custom_gain_FRDM_MCXN947/source/coeffs.cpp
@@ -26,11 +26,13 @@ void gain_offset_coeff( NAFE13388_UIM &afe, ref_points ref )
 	int32_t	offsset_coeff_cal	= afe.read_r24( OFFSET_COEFF + ref.cal_index );
 	int32_t	gain_coeff_new		= round( gain_coeff_cal * custom_gain );
 	int32_t	offset_coeff_new	= custom_offset - offsset_coeff_cal;
-	
+
+#if 0	
 	printf( "ref_point_high = %8ld @%6.3lf\r\n", ref.high.data, ref.high.voltage );
 	printf( "ref_point_low  = %8ld @%6.3lf\r\n", ref.low.data,  ref.low.voltage  );
 	printf( "gain_coeff_new   = %8ld\r\n", gain_coeff_new   );
 	printf( "offset_coeff_new = %8ld\r\n", offset_coeff_new );
+#endif
 	
 	afe.write_r24( GAIN_COEFF   + ref.coeff_index, gain_coeff_new   );
 	afe.write_r24( OFFSET_COEFF + ref.coeff_index, offset_coeff_new );
--- AFE_UIM_NAFE13388_A_custom_gain_FRDM_MCXN947/source/main.cpp
+++ AFE_UIM_NAFE13388_A_custom_gain_FRDM_MCXN947/source/main.cpp
@@ -14,25 +14,22 @@
 SPI				spi( D11, D12, D13, D10 );	//	MOSI, MISO, SCLK, CS
 NAFE13388_UIM	afe( spi );
 
+constexpr int	INPUT_GND			= 0x0010;
+constexpr int	INPUT_A1P_SINGLE	= 0x1010;
+
+constexpr int	CALIB_FOR_PGA_0_2	= 0;
+constexpr int	CALIB_NONE			= 8;
+constexpr int	CALIB_NONE_5V		= 9;
+constexpr int	CALIB_NONE_10V		= 10;
+constexpr int	CALIB_CUSTOM_5V		= 11;
+constexpr int	CALIB_CUSTOM_10V	= 12;
+constexpr int	CALIB_NONE_1V_5V	= 13;
+constexpr int	CALIB_CUSTOM_1V_5V	= 14;
+
 void	logical_ch_config_view( int channel );
 void	register16_dump( const std::vector<uint16_t> &reg_list );
-void	register24_dump( const std::vector<uint16_t> &reg_list );
+void	register24_dump( const std::vector<uint16_t> &reg_list, int cols = 4 );
 
-enum	output_type	{ RAW, MICRO_VOLT };
-
-constexpr	int	INPUT_GND			= 0x0010;
-constexpr	int	INPUT_A1P_SINGLE	= 0x1010;
-
-constexpr	int	CALIB_FOR_PGA_0_2	= 0;
-constexpr	int	CALIB_NONE			= 8;
-constexpr	int	CALIB_NONE_5V		= 9;
-constexpr	int	CALIB_NONE_10V		= 10;
-constexpr	int	CALIB_CUSTOM_5V		= 11;
-constexpr	int	CALIB_CUSTOM_10V	= 12;
-constexpr	int	CALIB_NONE_1V_5V	= 13;
-constexpr	int	CALIB_CUSTOM_1V_5V	= 14;
-
-using 	microvolt_t	= NAFE13388_UIM::microvolt_t;
 using 	raw_t		= NAFE13388_UIM::raw_t;
 
 PrintOutput	out( "test" );
@@ -61,8 +58,6 @@ int main( void )
 	afe.logical_ch_config( 12, INPUT_A1P_SINGLE, (CALIB_NONE_1V_5V   << 12) | 0x0084, 0x2900, 0x0000 );
 	afe.logical_ch_config( 13, INPUT_A1P_SINGLE, (CALIB_CUSTOM_1V_5V << 12) | 0x0084, 0x2900, 0x0000 );
 
-	constexpr float read_delay	= 0.01;
-
 	logical_ch_config_view( 0 );
 
 	//
@@ -74,13 +69,12 @@ int main( void )
 	for ( auto i = 0; i < 32; i++ )
 		registers_list[ i ]	= 0x80 + i;
 
-	out.printf( "=== before overwrite ===\r\n" );
+	out.printf( "=== GAIN_COEFF and OFFSET_COEFF registers before overwrite ===\r\n" );
 	register24_dump( registers_list );
 
 	//
 	//	gain/offset customization
 	//
-
 	
 	ref_points	r[]	= {
 		{ CALIB_NONE_5V,      { 2000,  5.0 }, {  0, 0.0 }, CALIB_NONE        },
@@ -94,7 +88,7 @@ int main( void )
 	for ( unsigned int i = 0; i < sizeof( r ) / sizeof( ref_points ); i++ )
 		gain_offset_coeff( afe, r[ i ] );
 
-	out.printf( "=== after overwrite ===\r\n" );
+	out.printf( "=== GAIN_COEFF and OFFSET_COEFF registers after overwrite ===\r\n" );
 	register24_dump( registers_list );
 
 	//
@@ -118,9 +112,10 @@ int main( void )
 
 	out.printf( "\r\n" );
 
-	long	count	= 0;
-	raw_t	data;
-	
+	raw_t			data;
+	long			count		= 0;
+	constexpr float read_delay	= 0.01;
+
 	while ( true )
 	{
 		out.printf( " %8ld, ", count++ );
@@ -156,12 +151,38 @@ void register16_dump( const std::vector<uint16_t> &reg_list )
 	);
 }
 
-void register24_dump( const std::vector<uint16_t> &reg_list )
+void register24_dump( const std::vector<uint16_t> &reg_list, int cols )
 {
+#if 0
 	for_each(
 		reg_list.begin(),
 		reg_list.end(),
 		//[]( auto reg ) { out.printf( "  0x%04X: 0x%06lX\r\n", reg, afe.read_r24( reg ) ); }
 		[]( auto reg ) { out.printf( "  0x%04X: %ld\r\n", reg, afe.read_r24( reg ) ); }
 	);
+#else
+	
+	const auto	length	= reg_list.size();
+	const auto	raws	= (length + cols - 1) / cols;
+	
+	for ( auto i = 0; i < raws; i++  )
+	{
+		if ( i )
+			out.printf( "\r\n" );
+			
+		for ( auto j = 0; j < cols; j++  )
+		{
+			auto	index	= i + j * raws;
+			
+			if ( index < length  )
+			{
+				auto	v	= reg_list[ i + j * raws ];
+				out.printf( "  %8ld @ 0x%04X", afe.read_r24( v ), v );			
+			}
+
+		}
+	}
+	
+	out.printf( "\r\n" );
+#endif
 }
