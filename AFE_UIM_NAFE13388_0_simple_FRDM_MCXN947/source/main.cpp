227e37f75b9727c97ce859f0c51a68efabc4acad
Tedd OKANO <okano.akifumi@gmail.com>
2025-02-26 05:54:43 +0900

    updated to sync latest version to "custom_gain" branch

--- AFE_UIM_NAFE13388_0_simple_FRDM_MCXN947/source/main.cpp
+++ AFE_UIM_NAFE13388_0_simple_FRDM_MCXN947/source/main.cpp
@@ -1,7 +1,7 @@
 /*
  *  @author Tedd OKANO
  *
- *  Released under the MIT license License
+ *  Released under the MIT license
  */
 
 #include	"r01lib.h"
@@ -10,25 +10,30 @@
 SPI				spi( D11, D12, D13, D10 );	//	MOSI, MISO, SCLK, CS
 NAFE13388_UIM	afe( spi );
 
-void	logical_ch_config_view( void );
-void	table_view( const std::vector<uint16_t> &reg_list, std::function<void(int)> func, int cols = 4 );
-
 enum	output_type	{ RAW, MICRO_VOLT };
 
 using 	microvolt_t	= NAFE13388_UIM::microvolt_t;
 using 	raw_t		= NAFE13388_UIM::raw_t;
 
+using enum	NAFE13388_UIM::Register16;
+using enum	NAFE13388_UIM::Register24;
+using enum	NAFE13388_UIM::Command;
+
+void	logical_ch_config_view( void );
+void	table_view( int size, int cols, std::function<void(int)> view, std::function<void(void)> linefeed = nullptr );
+
+
 int main( void )
 {
 	printf( "***** Hello, NAFE13388 UIM board! *****\r\n" );
 
-	spi.frequency( 1000 * 1000 );
+	spi.frequency( 1000'000 );
 	spi.mode( 1 );
 
 	afe.begin();
 
-	afe.logical_ch_config( 0, 0x1070, 0x0084, 0x2900, 0x0000 );
-	afe.logical_ch_config( 1, 0x2070, 0x0084, 0x2900, 0x0000 );
+	afe.logical_ch_config( 0, 0x1070, 0x3084, 0x2900, 0x0000 );
+	afe.logical_ch_config( 1, 0x2070, 0x3084, 0x2900, 0x0000 );
 
 	constexpr float read_delay	= 0.01;
 
@@ -63,18 +68,16 @@ int main( void )
 
 void logical_ch_config_view( void )
 {
-	uint16_t	en_ch_bitmap= afe.read_r16( 0x0024 );
+	uint16_t en_ch_bitmap	= afe.reg( CH_CONFIG4 );
 	
-	std::vector<uint16_t>	reg_list = { 0x0020, 0x0021, 0x0022, 0x0023 };
-
 	for ( auto channel = 0; channel < 16; channel++ )
 	{	
 		printf( "  logical channel %2d : ", channel );
 
 		if ( en_ch_bitmap & (0x1 << channel) )
 		{
-			afe.write_r16( channel );
-			table_view( reg_list, []( int v ){ printf(  "  0x%04X @ 0x%04X", afe.read_r16( v ), v ); }, 4 );
+			afe.command( channel );
+			table_view( 4, 4, []( int v ){ printf( "  0x%04X: 0x%04X", (uint16_t)(v + CH_CONFIG0), afe.reg( v + CH_CONFIG0 ) ); } );
 		}
 		else
 		{
@@ -83,24 +86,31 @@ void logical_ch_config_view( void )
 	}
 }
 
-void table_view( const std::vector<uint16_t> &reg_list, std::function<void(int)> func, int cols )
+void table_view( int length, int cols, std::function<void(int)> value, std::function<void(void)> linefeed )
 {
-	const auto	length	= (int)reg_list.size();
 	const auto	raws	= (int)(length + cols - 1) / cols;
 	
 	for ( auto i = 0; i < raws; i++  )
 	{
 		if ( i )
-			printf( "\r\n" );
-			
+		{
+			if ( linefeed )
+				linefeed();
+			else
+				printf( "\r\n" );
+		}
+		
 		for ( auto j = 0; j < cols; j++  )
 		{
 			auto	index	= i + j * raws;
 			
 			if ( index < length  )
-				func( reg_list[ index ] );
+				value( index );
 		}
 	}
 	
-	printf( "\r\n" );
+	if ( linefeed )
+		linefeed();
+	else
+		printf( "\r\n" );
 }
